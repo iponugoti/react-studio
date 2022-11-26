60695835945d84f920125248319f2493fb2eaf6d
iponugoti <isha_ponugoti@brown.edu>
2022-11-26 00:33:19 -0500

    bakery item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
+
+// bakeryData.forEach((item) => {
+//   item.image = "iponugoti.github.io/react-studio/" + item.image;
+// });
+
 /* ############################################################## */
 
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-  const [cart, setCart] = useState(0);  
+  // const [cart, setCart] = useState(0);  
 
   return (
     <div className="App">
       <h1>Isha's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item) => ( // TODO: map bakeryData to BakeryItem components
+        <p>
+          <BakeryItem name = {item.name} image = {item.image}/>
+        </p> // replace with BakeryItem component
       ))}
 
       <div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,8 +1,11 @@
 // TODO: create a component that displays a single bakery item
 export default function BakeryItem(props) {
     return(
-        <div>
-        HTML GOES HERE
-        </div>
+    <div> 
+      <h2>
+          {props.name}
+      </h2>        
+      <img src={props.image} alt = {props.name} class="preview"></img>
+    </div>
     )
 }
