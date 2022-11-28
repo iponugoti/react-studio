0aadb60464da6b6eb48f14c648d26f1ef5107434
iponugoti <isha_ponugoti@brown.edu>
2022-11-27 19:03:15 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 import BakeryItem from "./components/BakeryItem";
 
@@ -12,10 +12,36 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-  const [cart, setCart] = useState(0);  
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [cart, setCart] = useState([]);
 
-  const setCart = (name, price) => {
+  const addPrice = (price) => {
+    setTotalPrice((p) => price + p);
   };
+
+  const addItemToCart = (name, price) => {
+    const add = cart.map((c) => {
+      if (c.name === name) {
+        return {name: c.name, price: c.price, quantity: (c.quantity + 1)};
+      }
+      else {
+        return {name: c.name, price: c.price, quantity: 1};
+      }
+    });
+    setCart(add);
+    addPrice(price);
+  }
+
+
+  // const addItemToCart = (name, price) => {
+  //   if (cart.includes(name)) {
+  //     setCart((c) => [...c, {name: name, price: price, quantity: 2}]);
+  //   }
+  //   else {
+  //     setCart((c) => [...c, {name: name, price: price, quantity: 1}])
+  //   };
+  //   addPrice(price);
+  // };
 
   return (
     <div className="App">
@@ -23,14 +49,17 @@ function App() {
 
       {bakeryData.map((item) => ( // TODO: map bakeryData to BakeryItem components
         <p>
-          <BakeryItem name = {item.name} image = {item.image} desc = {item.description} price = {item.price}/>
+          <BakeryItem name = {item.name} image = {item.image} desc = {item.description} price = {item.price} addToCart = {addItemToCart}/>
         </p> // replace with BakeryItem component
       ))}
 
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-        
+        <h2>Cart ({cart.length})</h2>
+        {cart.map((item) => (
+        <p>
+          {item.quantity}x {item.name} ${item.price}
+        </p> ))}
+        Subtotal: ${totalPrice.toFixed(2)}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -2,16 +2,17 @@ import "./BakeryItem.css";
 
 // TODO: create a component that displays a single bakery item
 export default function BakeryItem(props) {
+    const add = props.addToCart;
     return(
     <div className="itemCard">
         <div className="image-container">
-            <img src={props.image} />
+            <img src={props.image} alt = {props.name} />
         </div>
         <div className="desc-container">
             <div className="name">{props.name}</div>
             <div className="desc">{props.desc}</div>
             <div className="price">${props.price}</div>
-            <button onClick={props.addToCart} className="addToCart">
+            <button onClick={() => (add(props.name, props.price))} className="addButton">
             Add to Cart
             </button>
         </div>
